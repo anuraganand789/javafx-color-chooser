4cb8b1b0c0d499de995aa6b36b54753282728643
Anurag Anand <anuraganand789@hotmail.com>
2020-11-19 23:23:22 +0530

    contains variables to represent javafx controls

---
+++ ColorChooserControls.java
@@ -0,0 +1,39 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+public class ColorChooserControls{
+    @FXML
+    protected Slider redSlider;
+
+    @FXML
+    protected Slider greenSlider;
+
+    @FXML
+    protected Slider blueSlider;
+
+    @FXML
+    protected Slider alphaSlider;
+
+    @FXML
+    protected TextField redTextField;
+
+    @FXML
+    protected TextField greenTextField;
+
+    @FXML
+    protected TextField blueTextField;
+
+    @FXML
+    protected TextField alphaTextField;
+
+    @FXML
+    protected Circle colorCircle;
+
+    @FXML
+    protected Rectangle colorRectangle;
+
+}
+
