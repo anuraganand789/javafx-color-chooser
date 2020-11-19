469b98ed1c0cba2a15868ef4289849f169dcda70
Anurag Anand <anuraganand789@hotmail.com>
2020-11-19 23:32:56 +0530

    as all of these classes are used only once in the code, so I have inlined all the imports for fun, to see how code looks like

--- ColorChooser.java
+++ ColorChooser.java
@@ -1,19 +1,11 @@
-import java.io.IOException;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class ColorChooser extends Application{
+public class ColorChooser extends javafx.application.Application{
     
     @Override
-    public void start(final Stage stage){
+    public void start(final javafx.stage.Stage stage){
 	stage.setTitle("Color Chooser");
 	try{
-	    stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("fxml/color-chooser.fxml"))));
-	}catch(final IOException ex){
+	    stage.setScene(new javafx.scene.Scene(javafx.fxml.FXMLLoader.load(getClass().getResource("fxml/color-chooser.fxml"))));
+	}catch(final java.io.IOException ex){
 	    ex.printStackTrace();
 	}
 	stage.show();
